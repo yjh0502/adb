c798027b74057e1bd3d0988257b2aed5af260010
Jihyun Yu <j.yu@navercorp.com>
2015-07-25 03:29:12 +0900

    remove coordiation translation on input

--- input.go
+++ input.go
@@ -5,20 +5,12 @@ import (
 	"strconv"
 )
 
-// TODO: landscape-left와 landscape-right의 좌표 translation이 다릅니다
-func translate(pt image.Point) image.Point {
-	w := lastImage.Bounds().Max.X
-	return image.Pt(pt.Y, w-pt.X)
-}
-
 func Tap(x, y int) error {
 	return TapPoint(image.Pt(x, y))
 }
 
 func TapPoint(pt image.Point) error {
-	trans := translate(pt)
-
-	args := []string{strconv.Itoa(trans.X), strconv.Itoa(trans.Y)}
+	args := []string{strconv.Itoa(pt.X), strconv.Itoa(pt.Y)}
 	return execWith(tapCommand(), args)
 }
 
@@ -27,14 +19,11 @@ func Back() error {
 }
 
 func Swipe(from, to image.Point) error {
-	transFrom := translate(from)
-	transTo := translate(to)
-
 	args := []string{
-		strconv.Itoa(transFrom.X),
-		strconv.Itoa(transFrom.Y),
-		strconv.Itoa(transTo.X),
-		strconv.Itoa(transTo.Y),
+		strconv.Itoa(from.X),
+		strconv.Itoa(from.Y),
+		strconv.Itoa(to.X),
+		strconv.Itoa(to.Y),
 	}
 	return execWith(swipeCommand(), args)
 }
